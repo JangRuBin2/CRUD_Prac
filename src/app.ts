94d0f770531aa0c0e2641e77485e96200b7b5420
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-18 14:42:54 +0900

    수정

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,10 @@ import mongoose, { Document, Schema } from 'mongoose';
 import path from 'path';
 
 const app = express();
-const port = 3000;
-
+//요청 본문 내용 json 파싱
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'src')));
+//mongoDB 연결
 mongoose.connect('mongodb://localhost:27017/test_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -17,7 +16,7 @@ interface MessageDocument extends Document {
   title: string;
   content: string;
 }
-
+// 스키마 생성
 const messageSchema = new Schema<MessageDocument>({
   title: String,
   content: String
@@ -40,6 +39,6 @@ app.post('/sendText', async (req: Request, res: Response) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(3000, () => {
+  console.log('Server is running');
 });
